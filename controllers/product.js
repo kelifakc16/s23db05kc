53b943d0a9abf05b5eac4e3cc5c2517ad26f56fd
kelifakc16 <kelifakc@gmail.com>
2023-11-10 10:25:03 -0600

    product

--- controllers/product.js
+++ controllers/product.js
@@ -12,10 +12,6 @@ exports.product_list = async function (req, res) {
 
 };
 
-// for a specific Product. 
-exports.product_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Product detail: ' + req.params.id);
-};
 // Handle Product create on POST. 
 exports.product_create_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Product create POST');
@@ -23,10 +19,6 @@ exports.product_create_post = function (req, res) {
 // Handle Product delete form on DELETE. 
 exports.product_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Product delete DELETE ' + req.params.id);
-};
-// Handle Product update form on PUT. 
-exports.product_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Product update PUT' + req.params.id);
 };
 
 
@@ -41,3 +33,34 @@ exports.product_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+// for a specific Product.
+exports.product_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Product.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+//Handle Product update form on PUT.
+exports.product_update_put = async function (req, res) {
+    try {
+        // console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+        let toUpdate = await Product.findById(req.params.id)
+
+        // // Do updates of properties
+        if (req.body.name) { toUpdate.name = req.body.name };
+        if (req.body.category) { toUpdate.category = req.body.category };
+        if (req.body.price) { toUpdate.price = req.body.price };
+        let result = await toUpdate.save();
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(' {"error": $ {err}: Update for id $ {req.params.id} failed');
+    }
+};
+
--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,10 @@
 var express = require('express');
-const product_controlers= require('../controllers/product');
+const product_controlers = require('../controllers/product');
 var router = express.Router();
 /* GET product */
-router.get('/', product_controlers.product_view_all_Page ); module.exports = router;
+router.get('/', product_controlers.product_view_all_Page);
+
+/* GET create product page */
+router.put('/product/:id', product_controlers.product_update_put);
+
+module.exports = router;

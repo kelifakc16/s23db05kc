170dd90b1f9a2470fb4703099c0c5311e3c4d545
kelifakc16 <kelifakc@gmail.com>
2023-11-17 08:53:17 -0600

    password

--- app.js
+++ app.js
@@ -3,7 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 var Product = require('./models/product');
+
+
+passport.use(new LocalStrategy(function (username, password, done) {
+  Account.findOne({ username: username }).then(function (user) {
+    if (err) { return done(err); } if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (!user.validPassword(password)) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+  })
+    .catch(function (err) {
+      return done(err)
+    })
+})
+)
 
 require('dotenv').config();
 const connectionString =
@@ -74,7 +97,7 @@ var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
 var resourceRouter = require('./routes/resource');
 var productRouter = require('./routes/product');
- 
+
 
 var app = express();
 
@@ -86,6 +109,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize()); app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

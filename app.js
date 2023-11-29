a4caec384abd02e9b72eac61ee237b9043247e76
kelifakc16 <kelifakc@gmail.com>
2023-11-28 21:43:21 -0600

    authentication

--- app.js
+++ app.js
@@ -1,94 +1,97 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var Product = require('./models/product');
+var Account = require('./models/account');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var Account = require('./models/account');
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
-var Product = require('./models/product');
 
-
-passport.use(new LocalStrategy(function (username, password, done) {
-  Account.findOne({ username: username }).then(function (user) {
-    if (err) { return done(err); } if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
-    }
-    if (!user.validPassword(password)) {
-      return done(null, false, { message: 'Incorrect password.' });
-    }
-    return done(null, user);
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
   })
-    .catch(function (err) {
-      return done(err)
-    })
-})
-)
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  );
 
-require('dotenv').config();
-const connectionString =
-  process.env.MONGO_CON
-mongoose = require('mongoose');
+
+
+require("dotenv").config();
+const connectionString = process.env.MONGO_CON;
+mongoose = require("mongoose");
 mongoose.connect(connectionString);
 
 //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.on("error", console.error.bind(console, "MongoDB connectionerror:"));
 db.once("open", function () {
-  console.log("Connection to DB succeeded")
+  console.log("Connection to DB succeeded");
 });
 
 // We can seed the collection if needed on server start
 async function recreateDB() {
 
-  // Delete everything
-  await Product.deleteMany();
-
-  let instance1 = new
-    Product({
-      name: "Bookshelf",
-      category: "Home Decor",
-      price: 10
+    // Delete everything
+    await Product.deleteMany();
+  
+    let instance1 = new
+      Product({
+        name: "Bookshelf",
+        category: "Home Decor",
+        price: 10
+      });
+    let instance2 = new
+      Product({
+        name: "Desk",
+        category: "Furniture",
+        price: 15
+      });
+    let instance3 = new
+      Product({
+        name: "Mirror",
+        category: "Home Decor",
+        price: 5
+      });
+  
+    instance1.save().then(doc => {
+      console.log("First object saved")
+    }
+    ).catch(err => {
+      console.error(err)
     });
-  let instance2 = new
-    Product({
-      name: "Desk",
-      category: "Furniture",
-      price: 15
+    instance2.save().then(doc => {
+      console.log("Second object saved")
+    }
+    ).catch(err => {
+      console.error(err)
     });
-  let instance3 = new
-    Product({
-      name: "Mirror",
-      category: "Home Decor",
-      price: 5
+    instance3.save().then(doc => {
+      console.log("Third object saved")
+    }
+    ).catch(err => {
+      console.error(err)
     });
-
-  instance1.save().then(doc => {
-    console.log("First object saved")
   }
-  ).catch(err => {
-    console.error(err)
-  });
-  instance2.save().then(doc => {
-    console.log("Second object saved")
-  }
-  ).catch(err => {
-    console.error(err)
-  });
-  instance3.save().then(doc => {
-    console.log("Third object saved")
-  }
-  ).catch(err => {
-    console.error(err)
-  });
+let reseed = true;
+if (reseed) {
+  recreateDB();
 }
 
-let reseed = true;
-if (reseed) { recreateDB(); }
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -102,10 +105,10 @@ var productRouter = require('./routes/product');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -113,9 +116,10 @@ app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
-}));
-app.use(passport.initialize()); app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -126,6 +130,10 @@ app.use('/product', productRouter);
 app.use('/resource', resourceRouter);
 
 
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -135,11 +143,12 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+
 
 module.exports = app;
--- models/account.js
+++ models/account.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require('passport-local-mongoose');
+
 const accountSchema = new Schema({
-    username: String,
-    password: String
-}); accountSchema.plugin(passportLocalMongoose);
-// We export the Schema to avoid attaching the model to the
-// default mongoose connection.
-module.exports = mongoose.model("Account", accountSchema)
+  username: String,
+  password: String
+});
+
+// Plugin passport-local-mongoose to add methods to the schema
+accountSchema.plugin(passportLocalMongoose);
+
+// Create and export the model
+module.exports = mongoose.model('Account', accountSchema);

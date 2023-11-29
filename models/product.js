cbc983bd383c0972e1c5a8f74b1a09be79d2f1cd
kelifakc16 <kelifakc@gmail.com>
2023-11-28 22:08:32 -0600

    bonus

--- models/product.js
+++ models/product.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose")
+
 const productSchema = mongoose.Schema({
-    name: String,
-    category: String,
-    price: Number
+    name: {
+        type: String,
+        required: true,
+        length:20,  
+
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, "Price cannot be negative"],
+        max: [1000, "Price cannot be greater than 1000"] 
+    }
 })
-module.exports = mongoose.model("Product",
-    productSchema)
+
+module.exports = mongoose.model("Product", productSchema)
--- routes/product.js
+++ routes/product.js
@@ -1,18 +1,31 @@
 var express = require('express');
 const product_controlers = require('../controllers/product');
 var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
+    
+
 /* GET product */
 router.get('/', product_controlers.product_view_all_Page);
 
 /* GET create product page */
-router.put('/product/:id', product_controlers.product_update_put);
+router.put('/product/:id',secured, product_controlers.product_update_put);
 // GET detail costume page */
-router.get('/detail', product_controlers.product_view_one_Page);
+router.get('/detail',secured, product_controlers.product_view_one_Page);
 /* GET create Product page */
-router.get('/create', product_controlers.product_create_Page);
+router.get('/create',secured, product_controlers.product_create_Page);
 /* GET create update page */
-router.get('/update', product_controlers.product_update_Page);
+router.get('/update',secured, product_controlers.product_update_Page);
 /* GET delete Product page */
-router.get('/delete', product_controlers.product_delete_Page);
+router.get('/delete',secured, product_controlers.product_delete_Page);
+ 
+
+ 
+ 
 
 module.exports = router;
